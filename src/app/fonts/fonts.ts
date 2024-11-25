5e2c7c7bd16fd3f681f9f3adaedaea8db4085b86
nikita <nikita.ratkevich91@mail.ru>
2024-11-25 18:13:20 +0200

    init project

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,8 @@
+import { Bai_Jamjuree, Asap, Noto_Sans } from "next/font/google";
+
+export const baiJamjuree = Bai_Jamjuree({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-bai-jamjuree",
+  display: "swap",
+});
