5f3206424af6d54f150b71c534c9d36c0f136153
nikita <nikita.ratkevich91@mail.ru>
2024-11-26 16:27:34 +0200

    add files

---
+++ src/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import MainImage from '@/../public/images/main.webp';
+import {shop} from "@/app/shared/consts/shop";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="font-bai-jamjuree text-[#fff] h-full">
+      <div className="max-w-[360px] bg-[#121423] w-full mx-auto sm:max-w-[100vw]">
+        <div className="sticky z-[10] p-[15px] w-full left-0 top-0 backdrop-blur-[5px] flex items-center justify-between pb-[15px] gap-[30px] sm:p-[4.167vw] sm:gap-[8.333vw] sm:pb-[4.167vw]">
+          <div className="flex items-center gap-[8px] sm:gap-[2.222vw] ">
+            <div className="relative shadow-custom-purple overflow-hidden w-[32px] h-[32px] rounded-[8px] sm:w-[8.889vw] sm:h-[8.889vw] sm:rounded-[2.222vw]">
+              <Image
+                fill
+                src={MainImage.src}
+                className="object-cover"
+                alt="image"
+              />
+            </div>
+            <p className="font-medium text-[12px] sm:text-[3.333vw]">
+              Get the AiGo and snag your girl today!
+            </p>
+          </div>
+          <Link className="bg-button-gradient shadow-custom-purple font-bold text-[12px] text-[#fff] px-[12px] py-[4px] rounded-[12px] sm:text-[3.333vw] sm:px-[3.333vw] sm:py-[1.2vw] sm:rounded-[3.333vw]" href="#">Get</Link>
+        </div>
+        <div className="gap-x-[12px] p-[15px] flex flex-wrap gap-y-[8px] sm:gap-y-[2.222vw] sm:gap-x-[3.333vw]">
+          {shop.map(item => {
+            return <div className="relative w-[48.1%] h-[206px] flex flex-col shadow-custom-purple inner-shadow justify-end shadow-inner-image rounded-[16px] p-[12px] sm:rounded-[4.444vw] sm:p-[3.333vw] sm:w-[43.689vw] sm:h-[57.222vw]" key={item.id}>
+              <Image
+                fill
+                src={item.image.src}
+                className="object-cover rounded-[16px] sm:rounded-[4.444vw]"
+                alt={item.name}
+              />
+              <div className="relative z-[5]">
+                <p className="text-[16px] tracking-[0.02em] font-bold sm:text-[4.444vw] leading-[1.2em]">{item.name}</p>
+                <p className="font-medium text-[12px] sm:text-[3.333vw]">{item.description}</p>
+              </div>
+            </div>
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/shared/consts/shop.ts
@@ -0,0 +1,276 @@
+import Avatar1 from '@/../public/images/1.webp';
+import Avatar2 from '@/../public/images/2.webp';
+import Avatar3 from '@/../public/images/3.webp';
+import Avatar4 from '@/../public/images/4.webp';
+import Avatar5 from '@/../public/images/5.webp';
+import Avatar6 from '@/../public/images/6.webp';
+import Avatar7 from '@/../public/images/7.webp';
+import Avatar8 from '@/../public/images/8.webp';
+import Avatar9 from '@/../public/images/9.webp';
+import Avatar10 from '@/../public/images/10.webp';
+import Avatar11 from '@/../public/images/11.webp';
+import Avatar12 from '@/../public/images/12.webp';
+import Avatar13 from '@/../public/images/13.webp';
+import Avatar14 from '@/../public/images/14.webp';
+import Avatar15 from '@/../public/images/15.webp';
+import Avatar16 from '@/../public/images/16.webp';
+import Avatar17 from '@/../public/images/17.webp';
+import Avatar18 from '@/../public/images/18.webp';
+import Avatar19 from '@/../public/images/19.webp';
+import Avatar20 from '@/../public/images/20.webp';
+import Avatar21 from '@/../public/images/21.webp';
+import Avatar22 from '@/../public/images/22.webp';
+import Avatar23 from '@/../public/images/23.webp';
+import Avatar24 from '@/../public/images/24.webp';
+interface IShop {
+  name: string
+  description: string
+  id: number
+  image: {
+    width: number
+    height: number
+    src: string
+  }
+}
+export const shop:IShop[] = [
+  {
+    name: "Sienna",
+    description: "Your flirty big sister",
+    id: 1,
+    image: {
+      width: Avatar1.width,
+      height: Avatar1.height,
+      src: Avatar1.src
+    }
+  },
+  {
+    name: "Ms. Vivienne Carter",
+    description: "Your seductive mentor ",
+    id: 2,
+    image: {
+      width: Avatar2.width,
+      height: Avatar2.height,
+      src: Avatar2.src
+    }
+  },
+  {
+    name: "Samantha Blake",
+    description: "Your naughty secretary",
+    id: 3,
+    image: {
+      width: Avatar3.width,
+      height: Avatar3.height,
+      src: Avatar3.src
+    }
+  },
+  {
+    name: "Ex-girlfriend",
+    description: "Your tempting past",
+    id: 4,
+    image: {
+      width: Avatar4.width,
+      height: Avatar4.height,
+      src: Avatar4.src
+    }
+  },
+  {
+    name: "Miyuki Neko",
+    description: "Your playful kitten",
+    id: 5,
+    image: {
+      width: Avatar5.width,
+      height: Avatar5.height,
+      src: Avatar5.src
+    }
+  },
+  {
+    name: "Mia",
+    description: "Sweet girl next door",
+    id: 6,
+    image: {
+      width: Avatar6.width,
+      height: Avatar6.height,
+      src: Avatar6.src
+    }
+  },
+  {
+    name: "Officer Mia Steele",
+    description: "Your flirty police officer",
+    id: 7,
+    image: {
+      width: Avatar7.width,
+      height: Avatar7.height,
+      src: Avatar7.src
+    }
+  },
+  {
+    name: "Diana Gadot",
+    description: "Your Wonder Woman",
+    id: 8,
+    image: {
+      width: Avatar8.width,
+      height: Avatar8.height,
+      src: Avatar8.src
+    }
+  },
+  {
+    name: "Hermione Granger",
+    description: "Your sultry witch",
+    id: 9,
+    image: {
+      width: Avatar9.width,
+      height: Avatar9.height,
+      src: Avatar9.src
+    }
+  },
+  {
+    name: "Cat Woman",
+    description: "Your Purr Passion",
+    id: 10,
+    image: {
+      width: Avatar10.width,
+      height: Avatar10.height,
+      src: Avatar10.src
+    }
+  },
+  {
+    name: "Princess Leia",
+    description: "Seductive princess",
+    id: 11,
+    image: {
+      width: Avatar11.width,
+      height: Avatar11.height,
+      src: Avatar11.src
+    }
+  },
+  {
+    name: "Fitness Trainer Lexi",
+    description: "Fitness goddess",
+    id: 12,
+    image: {
+      width: Avatar12.width,
+      height: Avatar12.height,
+      src: Avatar12.src
+    }
+  },
+  {
+    name: "Pocahontas",
+    description: "Wild Heart",
+    id: 13,
+    image: {
+      width: Avatar13.width,
+      height: Avatar13.height,
+      src: Avatar13.src
+    }
+  },
+  {
+    name: "Jasmine Al-Farooq",
+    description: "Exotic dream girl",
+    id: 14,
+    image: {
+      width: Avatar14.width,
+      height: Avatar14.height,
+      src: Avatar14.src
+    }
+  },
+  {
+    name: "Daenerys Targaryen",
+    description: "Fiery dragon queen",
+    id: 15,
+    image: {
+      width: Avatar15.width,
+      height: Avatar15.height,
+      src: Avatar15.src
+    }
+  },
+  {
+    name: "Lana Blaze",
+    description: "Dominant girl",
+    id: 16,
+    image: {
+      width: Avatar16.width,
+      height: Avatar16.height,
+      src: Avatar16.src
+    }
+  },
+  {
+    name: "Ava",
+    description: "Enigmatic Muse",
+    id: 17,
+    image: {
+      width: Avatar17.width,
+      height: Avatar17.height,
+      src: Avatar17.src
+    }
+  },
+  {
+    name: "Zoe",
+    description: "The Spirited Socialite",
+    id: 18,
+    image: {
+      width: Avatar18.width,
+      height: Avatar18.height,
+      src: Avatar18.src
+    }
+  },
+  {
+    name: "Brooke",
+    description: "The Glamorous Diva",
+    id: 19,
+    image: {
+      width: Avatar19.width,
+      height: Avatar19.height,
+      src: Avatar19.src
+    }
+  },
+  {
+    name: "Natalia",
+    description: "Office Assistant",
+    id: 20,
+    image: {
+      width: Avatar20.width,
+      height: Avatar20.height,
+      src: Avatar20.src
+    }
+  },
+  {
+    name: "Piper",
+    description: "Anime-lover",
+    id: 21,
+    image: {
+      width: Avatar21.width,
+      height: Avatar21.height,
+      src: Avatar21.src
+    }
+  },
+  {
+    name: "Raven",
+    description: "Seductive Cosplayer",
+    id: 22,
+    image: {
+      width: Avatar22.width,
+      height: Avatar22.height,
+      src: Avatar22.src
+    }
+  },
+  {
+    name: "Emiko",
+    description: "The Gentle Soul",
+    id: 23,
+    image: {
+      width: Avatar23.width,
+      height: Avatar23.height,
+      src: Avatar23.src
+    }
+  },
+  {
+    name: "Jacqueline",
+    description: "Your Friend's Cool Mom",
+    id: 24,
+    image: {
+      width: Avatar24.width,
+      height: Avatar24.height,
+      src: Avatar24.src
+    }
+  },
+]
